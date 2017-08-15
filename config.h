c3a9816aa2c36579e74b95bace49e5c53fd2efa7
Robert Swiecki <robert@swiecki.net>
2017-08-15 22:29:53 +0200

    v1.6

--- config.h
+++ config.h
@@ -10,7 +10,7 @@
 #define _CONFIG_H_
 
 #define INTRACE_NAME "InTrace"
-#define INTRACE_VERSION "1.6rc1"
+#define INTRACE_VERSION "1.6"
 #define INTRACE_AUTHORS "(C)2007-2016 Robert Swiecki <robert@swiecki.net>"
 
 #define MAX_HOPS 32
